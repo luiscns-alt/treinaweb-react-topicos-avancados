a9d5c8d26e3cb9c352dd2e96587f252be6a2bcac
Luis Carlos <luiscarlosnors@gmail.com>
2022-04-10 00:20:19 -0300

    Projeto: Telas da Aplicação

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,14 +1,39 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
+
 import NewProductView from './views/NewProductView';
 import ProductListView from './views/ProductListView';
 
-function App() {
-    return (
-        <div className='App'>
-            <NewProductView />
-            <ProductListView />
-        </div>
-    );
+class App extends Component {
+    render() {
+        return (
+            <div className='App'>
+                <Router>
+                    <div>
+                        <header>
+                            <ul className='link-list'>
+                                <li>
+                                    <Link to={'/'}>Novo</Link>
+                                </li>
+                                <li>
+                                    <Link to={'/list'}>Lista</Link>
+                                </li>
+                            </ul>
+                        </header>
+                        <div>
+                            <Route
+                                path={'/'}
+                                exact
+                                component={NewProductView}
+                            />
+                            <Route path={'/list'} component={ProductListView} />
+                        </div>
+                    </div>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
