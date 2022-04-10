3816efef178238aae7b8252fa595089d87ed33a1
Luis Carlos <luiscarlosnors@gmail.com>
2022-04-10 00:15:42 -0300

    Utilizando Hooks do React Router

--- ensino/src/App.jsx
+++ ensino/src/App.jsx
@@ -4,37 +4,47 @@ import {
     BrowserRouter as Router,
     Route,
     Link,
-    Routes,
-    Outlet,
+    Prompt,
+    Switch,
 } from 'react-router-dom';
 
 import Index from './views/index';
 import About from './views/about';
+import Address from './views/about/address';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
     render() {
+        const { state } = this;
         return (
             <div>
                 <Router>
                     <div>
+                        <Prompt
+                            when={false}
+                            message={(location) =>
+                                `Tem certeza que deseja ir para ${location.pathname}?`
+                            }
+                        />
                         <nav>
                             <ul>
                                 <li>
-                                    <Link tto='/about/abc'>Home</Link>
+                                    <Link to='/'>Home</Link>
                                 </li>
                                 <li>
-                                    <Link to='/about/def'>About</Link>
+                                    <Link to='/about/'>About</Link>
                                 </li>
                             </ul>
                         </nav>
-
-                        <Routes>
-                            <Route path='/' element={<Index />} />
-                            <Route
-                                path='/about/:minhaVariavel(abc|def)'
-                                element={<About />}
-                            />
-                        </Routes>
+                        <Switch>
+                            <Route path='/' exact component={Index} />
+                            <Route path='/about' component={About} />
+                            <Route path='/:abc' component={Address} />
+                        </Switch>
                     </div>
                 </Router>
             </div>
